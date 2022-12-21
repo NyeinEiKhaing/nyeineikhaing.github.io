10af0970dec4ccd39e62c002dde717b8b9600505
NyeinEiKhaing <nyeineikhaing123166@gmail.com>
2022-12-22 00:15:43 +0630

    a

--- Nyein_Clothing/js/custom.js
+++ Nyein_Clothing/js/custom.js
@@ -1,27 +1,27 @@
-
 $(document).ready(function(){
-    $('.my-slider').slick({
-      slidesToShow: 5,
+  $('.customer-logos').slick({
+      slidesToShow: 4,
       slidesToScroll: 1,
-      arrows: true,
-      dots: true,
-      speed: 300,
-      infinite: true,
-      autoplaySpeed: 5000,
       autoplay: true,
-      responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 3,
-      }
-    },
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 1,
-      }
-    }
-  ]
-    });
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 3
+          }
+      },{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 2
+        }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 1
+          }
+      }]
   });
+});
