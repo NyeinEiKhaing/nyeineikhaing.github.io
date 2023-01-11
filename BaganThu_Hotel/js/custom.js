55ed36b6a4de4267c90e762f149374aec8a724fc
NyeinEiKhaing <nyeineikhaing123166@gmail.com>
2023-01-11 19:09:04 +0630

    a

--- BaganThu_Hotel/js/custom.js
+++ BaganThu_Hotel/js/custom.js
@@ -31,3 +31,62 @@ $(document).ready(function () {
     });
 });
 // swap image end
+
+// revel start
+function reveal() {
+    var reveals = document.querySelectorAll(".zoomin");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+
+    var leftin = document.querySelectorAll(".leftin");
+  
+    for (var i = 0; i < leftin.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = leftin[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        leftin[i].classList.add("Leftin");
+      } else {
+        leftin[i].classList.remove("Leftin");
+      }
+    }
+  
+
+    var rightin = document.querySelectorAll(".rightin");
+  
+    for (var i = 0; i < rightin.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = rightin[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        rightin[i].classList.add("Rightin");
+      } else {
+        rightin[i].classList.remove("Rightin");
+      }
+    }
+}
+  
+  window.addEventListener("scroll", reveal);
+//   revel end
+
+// date placeholder start
+  // var date = new Date();
+  // var year = date.getFullYear();
+  // var month = date.getMonth()+1;
+  // var todayDate = String(date.getDate()).padStart(2,"0");
+  // var datePatten = year + '-' + month + "-" + todayDate;
+  // document.getElementById("date-picker").value = datePatten;
+  // document.write(datePatten);
+// date placeholder end
