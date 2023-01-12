3281e4a49bc874c79e8edf58abe57c0826c3581c
NyeinEiKhaing <nyeineikhaing123166@gmail.com>
2023-01-13 01:51:06 +0630

    a

--- BaganThu_Hotel/js/custom.js
+++ BaganThu_Hotel/js/custom.js
@@ -132,12 +132,3 @@ function myFunction2() {
 // read less read more end
 
 
-// date placeholder start
-  // var date = new Date();
-  // var year = date.getFullYear();
-  // var month = date.getMonth()+1;
-  // var todayDate = String(date.getDate()).padStart(2,"0");
-  // var datePatten = year + '-' + month + "-" + todayDate;
-  // document.getElementById("date-picker").value = datePatten;
-  // document.write(datePatten);
-// date placeholder end
